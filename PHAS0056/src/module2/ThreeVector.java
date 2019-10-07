1d9f43550003de8e3b2d2bb554ce1266d7942eac
Pip Benjamin <zcappbe@ucl.ac.uk>
2019-10-07 13:56:18 +0100

    Started module 2 ThreeVector

--- PHAS0056/src/module2/ThreeVector.java
+++ PHAS0056/src/module2/ThreeVector.java
@@ -1,10 +1,28 @@
 package module2;
 
 public class ThreeVector {
+	
+	int 
+	
+	ThreeVector vec = new ThreeVector(1, 2, 3); 
+	
+	//define a method to calculate the magnitude of a three-dimensional vector
+		public double magnitude (double x, double y, double z) {
+			
+			double mag;
+			mag=java.lang.Math.sqrt(x*x+y*y+z*z);
+			return mag;
+		}
+		
+		public double ThreeVector unitVector(magnitude) {
+			
+		}
+		
+	
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+	//}
 
 }
