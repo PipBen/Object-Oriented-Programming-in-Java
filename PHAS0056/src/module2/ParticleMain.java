1c4ad417cda236b0c93bac9e904bf6102cafed61
pipbe <pipbe@DESKTOP-6IDQ24O>
2019-10-14 02:01:13 +0100

    Only final comments to do

--- PHAS0056/src/module2/ParticleMain.java
+++ PHAS0056/src/module2/ParticleMain.java
@@ -4,11 +4,12 @@ public class ParticleMain {
 
 	public static void main(String[] args) {
 		FallingParticle FP=new FallingParticle(4.3,2.4);
-		FP.SetH(5);
+		FP.SetH(4.5);
 		FP.SetV(0);
-		FP.drop(1);
+		FP.drop(0.1);
 		System.out.println(FP.GetT());
 		System.out.println(FP.GetV());
+		System.out.println("When the time interval is 0.5s,the time taken for the particle to hit the bottom of");
 
 	}
 
