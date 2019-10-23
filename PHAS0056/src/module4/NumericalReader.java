c4e8194fe0cf782005a4cb39c1910255994cdf55
Pip Benjamin <zcappbe@ucl.ac.uk>
2019-10-23 12:26:35 +0100

    analysisStart complete,analyseData almost

--- PHAS0056/src/module4/NumericalReader.java
+++ PHAS0056/src/module4/NumericalReader.java
@@ -5,6 +5,14 @@ import java.util.*;
 
 public class NumericalReader {
 	
+	private double minValue;
+	private double maxValue;
+	private int nValues;
+	private double sumOfValues;
+	private String mainfile;
+	//double next_double;
+	
+	//returns user keyboard input
 	public static String getStringFromKeyboard() throws IOException{
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
@@ -16,6 +24,7 @@ public class NumericalReader {
 		return s;
 		}
 	
+	//takes the name of a URL and returns a BufferedReader object
 	public static BufferedReader brFromURL(String urlName) throws IOException {
 		URL u = new URL(urlName);
 		InputStream is = u.openStream();
@@ -24,17 +33,57 @@ public class NumericalReader {
 		return br;
 		}
 	
-	private void analysisStart(String dataFile) throws IOException{
+	//creates a file called dataFile and initializes variables
+	public void analysisStart(String dataFile) throws IOException{
 		FileWriter fw = new FileWriter(dataFile);
-		double minValue;
-		double maxValue;
-		double nValues;
-		double sumOfValues;
+		//BufferedWriter bw = new BufferedWriter(fw);
+		//PrintWriter pw = new PrintWriter(bw);
+		fw.close();
+		minValue=10000;
+		maxValue=-1000000;
+		nValues=0;
+		sumOfValues=0;
+		mainfile=dataFile;
 		
 	}
 	
+	
 	public void analyseData(String line) throws IOException{
+		Scanner s= new Scanner(line);
 		
+		
+		FileWriter fw = new FileWriter(mainfile);
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter pw = new PrintWriter(bw);
+		//double total=0;
+		while (s.hasNextLine() || s.hasNext()){
+			if (line.isEmpty() || ) {
+				s.nextLine();
+			}
+			else {
+				double next_double=s.nextDouble();
+				System.out.println(next_double);
+				pw.println(next_double);
+				
+				//update min value
+				if(next_double<minValue) {
+					minValue=next_double;
+				}
+				//update max value
+				if(next_double>maxValue) {
+					maxValue=next_double;
+				}
+				
+				//update number of values and sum of values
+				nValues=nValues+1;
+				sumOfValues=sumOfValues+next_double;
+				
+			}
+			
+		}
+		
+		pw.close();
+		s.close();
 	}
 	
 	
@@ -49,6 +98,14 @@ public class NumericalReader {
 			 catch (IOException e) {
 			 System.out.println("Problem: "+e.getMessage());
 			 }
+		NumericalReader nm = new NumericalReader();
+		
+		try{
+			nm.analyseData("10  5  6");
+		}
+		catch(IOException e) {
+			
+		}
 	}
 
 }
