afb4041748b36c24caaa5699a2df393a4c6f5b53
Pip Benjamin <zcappbe@ucl.ac.uk>
2019-10-28 15:57:14 +0000

    Started module 5

---
+++ PHAS0056/src/module5/SquareMatrix.java
@@ -0,0 +1,12 @@
+package module5;
+import java.lang.reflect.Array;
+
+public class SquareMatrix {
+	
+	public SquareMatrix(int n) {
+		double[][] matrix = new double[n][n];
+		int nRows = matrix.length;
+		int nCols = Array.getLength(matrix[0]);
+	}
+
+}
---
+++ PHAS0056/src/module5/TestSquareMatrix.java
@@ -0,0 +1,11 @@
+package module5;
+
+public class TestSquareMatrix {
+
+	public static void main(String[] args) {
+		SquareMatrix A = new SquareMatrix(2);
+		A= {{2,1,0},{0,1,0},{-1,0,2}};
+
+	}
+
+}
