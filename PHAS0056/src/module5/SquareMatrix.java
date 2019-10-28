48203162030410fba8a4a5b205e6dba0b65e673a
Pip Benjamin <zcappbe@ucl.ac.uk>
2019-10-28 16:25:38 +0000

    Initial constructor and testing

--- PHAS0056/src/module5/SquareMatrix.java
+++ PHAS0056/src/module5/SquareMatrix.java
@@ -3,10 +3,17 @@ import java.lang.reflect.Array;
 
 public class SquareMatrix {
 	
-	public SquareMatrix(int n) {
-		double[][] matrix = new double[n][n];
+	public SquareMatrix(double[][] elements) throws Exception {
+		double[][] matrix = elements;
 		int nRows = matrix.length;
 		int nCols = Array.getLength(matrix[0]);
+		if(nRows!= nCols) {
+			throw new Exception("input matrix is not square");
+		}
+	}
+	
+	public String toString() {
+		
 	}
 
 }
--- PHAS0056/src/module5/TestSquareMatrix.java
+++ PHAS0056/src/module5/TestSquareMatrix.java
@@ -1,10 +1,22 @@
 package module5;
 
+
 public class TestSquareMatrix {
 
 	public static void main(String[] args) {
-		SquareMatrix A = new SquareMatrix(2);
-		A= {{2,1,0},{0,1,0},{-1,0,2}};
+		double[][] A_comp= {{2,1,0},{0,1,0},{-1,0,2}};
+		double[][] B_comp= {{1,3,1},{0,2,0},{1,0,-1}};
+		
+		try {
+			SquareMatrix A = new SquareMatrix(A_comp);
+			SquareMatrix B = new SquareMatrix(B_comp);
+			System.out.println(A);
+			System.out.println(B);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		//A= {{2,1,0},{0,1,0},{-1,0,2}};
 
 	}
 
