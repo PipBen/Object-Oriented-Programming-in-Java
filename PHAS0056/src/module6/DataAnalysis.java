d96b0de752b52e4165c2a93fda8f5f0fbd5de233
pipbe <pipbe@10.97.126.183>
2019-11-20 12:01:58 +0000

    update to DataAnalysis

--- PHAS0056/src/module6/DataAnalysis.java
+++ PHAS0056/src/module6/DataAnalysis.java
@@ -30,8 +30,13 @@ public class DataAnalysis implements GoodnessOfFitCalculator{
 	public static void main(String[] args) {
 		try{
 			Collection<Object> collec = TestDataPoints.dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module6/module6-data.txt");
-			
-		
+			Theory pow2 = new PowerLawTheory(2);
+			Theory pow205 = new PowerLawTheory(2.05);
+			Theory quad210 = new QuadraticTheory(2,1,0);
+			ArrayList<Theory> theories = new ArrayList<Theory>();
+			theories.add(pow2);
+			theories.add(pow205);
+			theories.add(quad210);
 		}
 		catch(IOException e){
 			System.out.println("Problem: "+e.getMessage());
