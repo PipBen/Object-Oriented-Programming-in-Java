57b9ccd798c2466651cd36e3f12c1491ad63c57e
Pip Benjamin <zcappbe@ucl.ac.uk>
2020-01-22 14:45:45 +0000

    almost finished 2016 exam

--- PHAS0056/src/exam1516/DetectorStore.java
+++ PHAS0056/src/exam1516/DetectorStore.java
@@ -9,18 +9,28 @@ import java.util.ArrayList;
 public class DetectorStore {
 	
 	public ArrayList<Detector> detectors;
+	public ArrayList<DetectorThreshold> detectorThresholds;
+	public Detector maxDiffDetector;
 	
-	public DetectorStore(String urlName, SignalStore signalStore) throws Exception{
+	public DetectorStore(String urlName, SignalStore signalStore, ArrivalTimeCalculator arrivalTimeCalculator) throws Exception{
 		URL u = new URL(urlName);
 		InputStream is = u.openStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		String line=br.readLine();
 		this.detectors= new ArrayList<Detector>();
+		this.detectorThresholds= new ArrayList<DetectorThreshold>();
 		while (line!= null) {
-			Detector detector = new Detector(line);
-			detector.setSignalStore(signalStore);
-			detectors.add(detector);
+			if(arrivalTimeCalculator instanceof MaxVoltageArrivalTime) {
+				Detector detector = new Detector(line);
+				detector.setSignalStore(signalStore);
+				detectors.add(detector);
+			}
+			if(arrivalTimeCalculator instanceof ThresholdArrivalTime) {
+				DetectorThreshold detector = new DetectorThreshold(line);
+				detector.setSignalStore(signalStore);
+				detectorThresholds.add(detector);
+			}
 			line=br.readLine();
 		}
 	}
@@ -30,7 +40,54 @@ public class DetectorStore {
 	 */
 	@Override
 	public String toString() {
-		return  detectors.toString();
+		if(detectors.isEmpty()) {
+			return detectorThresholds.toString();
+		}
+		else {
+			return detectors.toString();
+		}
 	}
+
+	/**
+	 * @return the detectors
+	 */
+	public ArrayList<Detector> getDetectors() {
+		return detectors;
+	}
+
+	/**
+	 * @return the detectorThresholds
+	 */
+	public ArrayList<DetectorThreshold> getDetectorThresholds() {
+		return detectorThresholds;
+	}
+
+	/**
+	 * @param detectors the detectors to set
+	 */
+	public void setDetectors(ArrayList<Detector> detectors) {
+		this.detectors = detectors;
+	}
+
+	/**
+	 * @param detectorThresholds the detectorThresholds to set
+	 */
+	public void setDetectorThresholds(ArrayList<DetectorThreshold> detectorThresholds) {
+		this.detectorThresholds = detectorThresholds;
+	}
+	
+	public Detector getMaxDiffDetector() {
+		double maxDifference=0;
+		for(int n=0; n<detectors.size();n++) {
+			double maxAmpSpeed=detectors.get(n).getParticleSpeed();
+			double thresholdSpeed =detectorThresholds.get(n).getParticleSpeed();
+			double difference =Math.abs(maxAmpSpeed-thresholdSpeed);
+			if (difference>maxDifference) {
+				maxDifference=difference;
+				maxDiffDetector=detectors.get(n);
+			}
+		}
+		return maxDiffDetector;
+	}	
 
 }
--- PHAS0056/src/exam1516/Main1516.java
+++ PHAS0056/src/exam1516/Main1516.java
@@ -5,12 +5,16 @@ public class Main1516 {
 	public static void main(String[] args) throws Exception {
 		
 		SignalStore signalStore= new SignalStore ("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/signals.txt");
-		DetectorStore detectors = new DetectorStore("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/detectors.txt",signalStore);
+		ArrivalTimeCalculator maxVoltageArrivalTime = new MaxVoltageArrivalTime();
+		DetectorStore detectors = new DetectorStore("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/detectors.txt",signalStore, maxVoltageArrivalTime);
 		System.out.println(detectors);
 		
 		//ThresholdArrivalTime oneVolt= new ThresholdArrivalTime(1);
-		DetectorThresholdStore detectorThresholds = new DetectorThresholdStore("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/detectors.txt",signalStore);
+		ArrivalTimeCalculator thresholdArrivalTime = new ThresholdArrivalTime(1);
+		DetectorStore detectorThresholds = new DetectorStore("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/detectors.txt",signalStore, thresholdArrivalTime);
 		System.out.println(detectorThresholds);
+		
+		System.out.println(detectors.getMaxDiffDetector());
 	}
 
 }
